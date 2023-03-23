dc3abdf04873f98ad60f1bb0878f10d1abc7b63d
Kenneth Caple <kennethcaple@Kenneths-iMac-Pro.local>
2023-03-23 09:06:51 -0700

    really ready

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
+const dotenvconfig = require('dotenv').config();
+const config = {apiKey: process.env.OPENAI_API_KEY};
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration(config);
+const openai = new OpenAIApi(configuration);
+async function runCompletion (input) {
+    const completion = await openai.createCompletion({
+         model: "text-davinci-003",
+         prompt: input,
+         temperature: 0,
+         max_tokens: 1000,
+     });
+     return completion.data.choices[0].text;
+ }
 const express = require('express')
 const bodyParser = require('body-parser');
+const morgan = require('morgan')
 const cors = require('cors');
-const ai = require("./answer");
-
 const app = express()
 const port = 3000
 
+app.use(morgan('combined'))
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.get('/status', (req, res) => {
-    res.send('Server is up');
-});
-
+app.use(express.json()); // Required to parse JSON request body
 app.post('/api/request', async (req, res) => {
-    let prompt = req.body.prompt;
-    let completion = await ai.runCompletion(prompt);
-    res.json(completion);
+    try {
+        let prompt = req.body.prompt;
+        let completion = await runCompletion(prompt);
+        res.send(completion);
+      } catch (error) {
+        res.status(500).send({ error: error.message });
+      }
 });
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
