83bf43d3401887b34c2027e267f5cc916dcbc196
Kenneth Caple <kennethcaple@Kenneths-iMac-Pro.local>
2023-03-23 09:22:42 -0700

    ports

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan')
 const cors = require('cors');
 const app = express()
-const port =  process.env.PORT || 3000;
+const port =  process.env.PORT || 80;
 
 app.use(morgan('combined'))
 app.use(cors());
