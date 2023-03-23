e66b46defe343313739a5cdb206fc04c41112456
Kenneth Caple <kennethcaple@Kenneths-iMac-Pro.local>
2023-03-23 05:25:36 -0700

    ready

--- server.js
+++ server.js
@@ -1,31 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const ai = require("./answer");
 
 const app = express()
 const port = 3000
 
-
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/api', (req, res) => {
-    let request = req.body
-    res.json(request);
-}); 
-
-
-app.get('/api/:r1', (req, res) => {
-    // reading isbn from the URL
-    const r1 = req.params.r1;
-    res.json(r1);
+app.get('/status', (req, res) => {
+    res.send('Server is up');
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+app.post('/api/request', async (req, res) => {
+    let prompt = req.body.prompt;
+    let completion = await ai.runCompletion(prompt);
+    res.json(completion);
 });
-
 
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`));
